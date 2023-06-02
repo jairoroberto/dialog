18ce97dc99e23a83d89613ce951a54e9974f9581
Jairo Roberto <jairorob@gmail.com>
2023-06-02 10:14:19 -0300

    News Code

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const button = document.querySelector('.btn-abrir-modal');
+const btnOpen = document.querySelector('#btn-open-modal');
 const modal = document.querySelector('dialog');
 const buttonClose = document.querySelector('dialog button');
 
-button.onclick = function(){
+
+
+
+btnOpen.onclick = function(){
     modal.showModal();
 }
 
 buttonClose.onclick = function(){
     modal.close();
+    console.log('Click no fechar!');
 }
+
+const arr = ['Apple', 'Orange', 'Avocato']
+console.table(arr);
+
+
+
+// for(let i=0; i < arr.length; i++){
+//     console.log('Array Uni:', arr[i]);
+//     modal.innerHTML += `${arr[i]} <br>`;
+// }
+
 
 
 
